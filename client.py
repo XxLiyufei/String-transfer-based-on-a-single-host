9e45b6bbe2a320c0358ba6e65d1b1b7a15f7e40b
XxLiyufei <39880831+XxLiyufei@users.noreply.github.com>
2018-06-07 01:58:06 +0000

    Update client.py

--- client.py
+++ client.py
@@ -1,13 +1,32 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
-# 文件名：client.py
+#!/usr/bin/env python
+# -*- coding=utf-8 -*-
 
-import socket
 
-s=socket.socket()  #创建socket对象
-host=socket.gethostname()  #获取主机名
-port=12345  #设置端口号
+"""
+file: client.py
+socket client
+"""
 
-s.connect((host,port))
-print s.recv(1024)
-s.close
+import socket #socket模块
+import sys #sys模块
+
+
+def socket_client(): #创建客户端
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket.AF_INET建立服务器间网络通信，socket.SOCK_STREAM为流式socket，for TCP
+        s.connect(('127.0.0.1', 6666)) #连接到服务器，端口号为6666
+    except socket.error as msg: #防止socket server重启后端口被占用
+        print msg
+        sys.exit(1)
+    print s.recv(1024) #数据发送大小限定
+    while 1:
+        data = raw_input('请输入需要传输的字符: ')
+        s.send(data)
+        print s.recv(1024)
+        if data == 'exit':
+            break
+    s.close() #关闭连接
+
+
+if __name__ == '__main__':
+    socket_client()
